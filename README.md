47a58d349773541ff85b1e30fc5971dc58d0e91e
amjad2000in <amjad2000in@gmail.com>
2019-08-15 21:01:48 +0530

    looot of changes

--- README.md
+++
@@ -1,1 +0,0 @@
-# talkback-lambda
--- tberror/build_and_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -gt 0 ] ; then
-  STAGE=$1
-else
-  STAGE="stage"
-fi
-
-../cloudformation/build_and_deploy_function.sh tberror TBError $STAGE
